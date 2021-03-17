36d6a1d74c0a80195286733381c06106a4fbf5b5
AtariBot <d_wiet@o2.pl>
2021-03-17 18:52:46 +0000

    Header.js: updated Paragraph 1 in Paragraph

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends React.Component {
                     <span className="icon major fa-cloud"></span>
                     <h1>Welcome to our dev portal. <strong>Photon</strong>, another fine<br />
                     little freebie from <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                    <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
+                    <p>Learn about our APIs, webhooks and data feeds.<br />
                     lobortis feugiat sapien sed etiam volutpat accumsan.</p>
                     <ul className="actions">
                         <li><a href="#one" className="button scrolly">Discover</a></li>
