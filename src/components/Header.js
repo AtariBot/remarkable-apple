201857fbb3662373b466e111e91e51727ade4083
AtariBot <d_wiet@o2.pl>
2021-03-17 18:59:27 +0000

    Header.js: updated Paragraph 2 in Paragraph

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends React.Component {
                     <h1>Welcome to our dev portal. <strong>Photon</strong>, another fine<br />
                     little freebie from <a href="http://html5up.net">HTML5 UP</a>.</h1>
                     <p>Learn about our APIs, webhooks and data feeds.<br />
-                    lobortis feugiat sapien sed etiam volutpat accumsan.</p>
+                    Where magic happens</p>
                     <ul className="actions">
                         <li><a href="#one" className="button scrolly">Discover</a></li>
                     </ul>
